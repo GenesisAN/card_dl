bb5addd174ba6cd0a5c5507c734d9141630d37ba
AN_PC <Genesis_AN@outlook.com>
2023-11-06 02:07:27 +0800

    add tag

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,60 +1,147 @@
 <template>
-  <div class="product-page">
+  <div>
+    <!-- Product header will now use flexbox to align items -->
     <div class="product-header">
-      <div class="image-gallery">
-        <img :src="currentImage" alt="Game Cover" class="product-cover" />
-        <div class="gallery-controls">
-          <button
-            v-for="(image, index) in images"
-            :key="index"
-            @click="setCurrentImage(image)"
-          >
+      <!-- Carousel container with flex: 2 to take twice as much space as author info -->
+      <v-carousel>
+        <v-carousel-item>
+          <div class="d-flex justify-center align-center" style="height: 100%">
+            <img src="https://picsum.photos/1920/1080" />
+          </div>
+        </v-carousel-item>
+
+        <v-carousel-item>
+          <div class="d-flex justify-center align-center" style="height: 100%">
             <img
-              :alt="'Game image ' + (index + 1)"
-              :src="image"
-              class="thumbnail"
+              src="http://localhost:3000/api/v1/card/thumd/1/9df566e37275a8c93d3be31319a7588e.jpg"
             />
-          </button>
-        </div>
-      </div>
-      <h1 class="product-title">Game Title</h1>
+          </div>
+        </v-carousel-item>
+
+        <v-carousel-item>
+          <div class="d-flex justify-center align-center" style="height: 100%">
+            <img
+              src="http://localhost:3000/api/v1/card/thumd/1/397ec4ed8a6d80b9151a4686c9b70494.jpg"
+            />
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+      <!-- Author info container with flex: 1 -->
+      <h1 class="product-title">Card Title</h1>
       <p class="product-description">
-        Short description of the game, its features, and any important
-        information a buyer might want to know.
+        Short description of the card, its features, and any important
+        information a player might want to know.
       </p>
+      <div class="text-center">
+        <v-chip
+          class="ma-2"
+          color="indigo"
+          prepend-icon="mdi-account-circle"
+          text-color="white"
+        >
+          Mike
+        </v-chip>
+
+        <v-chip
+          append-icon="mdi-star"
+          class="ma-2"
+          color="orange"
+          text-color="white"
+        >
+          Premium
+        </v-chip>
+
+        <v-chip
+          append-icon="mdi-cake-variant"
+          class="ma-2"
+          color="primary"
+          text-color="white"
+        >
+          1 Year
+        </v-chip>
+
+        <v-chip class="ma-2" color="green" text-color="white">
+          <template v-slot:prepend>
+            <v-avatar class="green-darken-4"> 1 </v-avatar>
+          </template>
+          Years
+        </v-chip>
+
+        <v-chip
+          :model-value="true"
+          class="ma-2"
+          closable
+          color="teal"
+          prepend-icon="mdi-checkbox-marked-circle"
+          text-color="white"
+        >
+          Confirmed
+        </v-chip>
+
+        <v-chip
+          :model-value="true"
+          class="ma-2"
+          closable
+          close-icon="mdi-delete"
+          color="teal"
+          prepend-icon="mdi-checkbox-marked-circle"
+          text-color="white"
+        >
+          Confirmed
+        </v-chip>
+      </div>
       <div class="purchase-options">
-        <button class="buy-button">Add to Cart</button>
-        <div class="price">Price</div>
+        <button class="buy-button">Download</button>
+        <button class="buy-button">Add to Bookmark</button>
+        <button class="buy-button">Like</button>
+        <button class="buy-button">Report</button>
       </div>
     </div>
-    <div class="product-details">
-      <h2>Details</h2>
-      <p>
-        Here you can add more detailed information about the game, like system
-        requirements, languages supported, etc.
-      </p>
-    </div>
-    <div class="user-reviews">
-      <h2>User Reviews</h2>
-      <div class="review">
-        <h3>Username</h3>
-        <p>
-          This is a review by a user. It contains their opinion on the game.
-        </p>
+    <div class="product-page">
+      <div class="product-details">
+        <h2>Details</h2>
+        <p>Here you can add more detailed information about the card.</p>
       </div>
-      <!-- Repeat the above div for each review -->
+      <div class="user-reviews">
+        <h2>User Reviews</h2>
+        <!-- Assuming there are multiple reviews, so this would be repeated for each one. -->
+        <div class="review">
+          <h3>Another Username</h3>
+          <p>
+            This is another review by a user. It contains their opinion on the
+            card.
+          </p>
+        </div>
+        <!-- Repeat the above div for each review -->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ProductPage",
-  // Data and methods for the product page can be added here
+  name: "CardView",
 };
 </script>
 
 <style scoped>
+.product-header >>> ul.slick-dots {
+  padding-left: 0px;
+}
+
+.v-carousel .v-carousel-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%; /* 这将确保每个轮播项都有100%的高度 */
+}
+
+.v-carousel .v-carousel-item img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain; /* 这将确保图片不会被拉伸或压缩 */
+}
+
 .product-page {
   display: flex;
   flex-direction: column;
@@ -62,20 +149,12 @@ export default {
   font-family: Arial, sans-serif;
 }
 
+.product-title {
+  margin-top: 20px;
+}
+
 .product-header {
   text-align: center;
-}
-
-.product-cover {
-  width: 100%;
-  max-width: 600px; /* Adjust as necessary */
-  border-radius: 8px;
-  margin-bottom: 20px;
-}
-
-.product-title {
-  font-size: 2em;
-  margin: 10px 0;
 }
 
 .product-description {
@@ -109,7 +188,6 @@ export default {
   color: #333;
 }
 
-.product-details,
 .user-reviews {
   width: 100%;
   max-width: 800px; /* Adjust as necessary */
